c33135c06fcc666e6e27afcdccd81fbf3f12c039
zacanger <zac@zacanger.com>
2016-12-12 13:46:32 -0700

    example

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Watch from 'scrollmonitor-react'
+
+const App = () => (
+  <h1>{typeof Watch}</h1>
+)
+
+render(<App />, document.getElementById('root'))
